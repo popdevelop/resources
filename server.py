ac59867344cf90e0f6545ddce3b455e6764a598f
Johan Brissmyr <brissmyr@gmail.com>
2010-09-02 21:00:32 +0200

    Removed authorization

--- server.py
+++ server.py
@@ -6,7 +6,6 @@ from models import Line
 import database
 import logging
 import os.path
-import tornado.auth
 import tornado.escape
 import tornado.httpclient
 import tornado.httpserver
@@ -25,8 +24,6 @@ class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
             (r"/", MainHandler),
-            (r"/auth/login", AuthLoginHandler),
-            (r"/auth/logout", AuthLogoutHandler),
             (r"/a/message/new", MessageNewHandler),
             (r"/a/message/updates", MessageUpdatesHandler),
             (r"/neareststations", NearestStationsHandler),
@@ -35,9 +32,6 @@ class Application(tornado.web.Application):
         ]
         settings = dict(
             cookie_secret="61oETzKXnQAGaYdkL5gEmGeJJFuYh7EQnp2XdTP1o/Vo=",
-            twitter_consumer_key = "39DOr5gcrYUI6Hga71sgIg",
-            twitter_consumer_secret = "sOEV2NW00y6RX5aKzVfFslLjavKRZfLaUS6as1OwvE",
-            login_url="/auth/login",
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             xsrf_cookies=True,
@@ -53,13 +47,11 @@ class BaseHandler(tornado.web.RequestHandler):
 
 
 class MainHandler(BaseHandler):
-    @tornado.web.authenticated
     def get(self):
         self.render("index.html")
 
 
 class APIHandler(BaseHandler):
-#    @tornado.web.authenticated
     @tornado.web.asynchronous
     def get(self):
         self.args = dict(zip(self.request.arguments.keys(),
@@ -187,7 +179,6 @@ class MessageMixin(object):
 
 
 class MessageNewHandler(BaseHandler, MessageMixin):
-#    @tornado.web.authenticated
     def post(self):
         message = {
             "id": str(uuid.uuid4()),
@@ -203,7 +194,6 @@ class MessageNewHandler(BaseHandler, MessageMixin):
 
 
 class MessageUpdatesHandler(BaseHandler, MessageMixin):
-#    @tornado.web.authenticated
     @tornado.web.asynchronous
     def post(self):
         cursor = self.get_argument("cursor", None)
@@ -215,27 +205,6 @@ class MessageUpdatesHandler(BaseHandler, MessageMixin):
         if self.request.connection.stream.closed():
             return
         self.finish(dict(messages=messages))
-
-
-class AuthLoginHandler(BaseHandler, tornado.auth.GoogleMixin):
-    @tornado.web.asynchronous
-    def get(self):
-        if self.get_argument("openid.mode", None):
-            self.get_authenticated_user(self.async_callback(self._on_auth))
-            return
-        self.authenticate_redirect(ax_attrs=["name"])
-
-    def _on_auth(self, user):
-        if not user:
-            raise tornado.web.HTTPError(500, "Google auth failed")
-        self.set_secure_cookie("user", tornado.escape.json_encode(user))
-        self.redirect("/")
-
-
-class AuthLogoutHandler(BaseHandler):
-    def get(self):
-        self.clear_cookie("user")
-        self.write("You are now logged out")
 
 
 def main():
